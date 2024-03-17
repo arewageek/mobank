567f5d541aab234f92f05f058c2f03df42cf6238
arewageek <arewageek@gmail.com>
2024-03-17 16:31:21 +0100

    Color remodelled

--- src/components/modules/QuickTransfer.jsx
+++ src/components/modules/QuickTransfer.jsx
@@ -1,69 +1,70 @@
-import React from 'react'
-import { FaCaretDown, FaCcMastercard, FaPaperPlane } from 'react-icons/fa'
-import { AvailableCards } from './AvailableCards'
+import React from "react";
+import { FaCaretDown, FaCcMastercard, FaPaperPlane } from "react-icons/fa";
+import { AvailableCards } from "./AvailableCards";
 
 export const QuickTransfer = () => {
-    return (
-        <div>
-            <h3 className="font-bold text-[15pt] text-gray-800 mb-5">
-                Quick transfer
-            </h3>
+  return (
+    <div>
+      <h3 className="font-bold text-[15pt] text-gray-800 mb-5">
+        Quick transfer
+      </h3>
 
-            <div className="rounded-3xl bg-violet-100 flex flex-col space-y-5 px-6 py-20">
-                <div className="w-full flex items-center space-x-3">
-                    <input type="tel" className="text-[8.5pt] font-[450] px-5 py-3 border-2 border-gray-200 bg-gray-50 text-gray-600 placeholder:text-gray-400 w-full rounded-xl" placeholder="Account Number" />
+      <div className="rounded-3xl bg-slate-100 flex flex-col space-y-5 px-6 py-20">
+        <div className="w-full flex items-center space-x-3">
+          <input
+            type="tel"
+            className="text-[8.5pt] font-[450] px-5 py-3 border-2 border-gray-200 bg-gray-50 text-gray-600 placeholder:text-gray-400 w-full rounded-xl"
+            placeholder="Account Number"
+          />
 
-                    <button className="px-5 py-3 rounded-lg bg-indigo-900/90 text-sm text-indigo-50 hover:bg-black transition duration-300">
-                        <FaPaperPlane />
-                    </button>
-                </div>
-
-                <div className="w-full">
-                    <div className="rounded-xl border-2 border-gray-300 px-5 py-3 flex justify-between items-center text-[9pt]">
-                        <div className="flex items-center space-x-2">
-                            <span>
-                                <FaCcMastercard />
-                            </span>
-                            <span>
-                                Debit
-                            </span>
-                        </div>
-
-                        <div className="flex items-center space-x-2">
-                            <span>
-                                $10.432
-                            </span>
-                            <span>
-                                <FaCaretDown />
-                            </span>
-                        </div>
-                    </div>
-                </div>
-
-
-                <div className="w-full flex items-center space-x-3">
-                    <div className="text-[8.5pt] font-[450] h-[60pt] px-5 py-5 border-2 border-gray-200 bg-gray-50 text-gray-600  w-full rounded-xl">
-                        <label htmlFor="#amount">
-                            Account Number ($)
-                        </label>
-                        <input type="amount" className="h-full w-full text-lg font-bold text-gray-900 bg-transparent" placeholder="$124" />
-                    </div>
-                </div>
-
-                <div className="w-full flex justify-center space-x-3 font-bold text-sm">
-                    <button className="bg-violet-950 text-violet-50 px-4 py-2 w-full shadow-md rounded-lg hover:bg-violet-300 border-2 border-violet-950 hover:text-violet-950 transition duration-300">
-                        Send Money
-                    </button>
-
-                    <button className="bg-violet-50 text-violet-950 px-4 py-2 w-full shadow-md rounded-lg hover:bg-violet-300 border-2 border-violet-950 hover:text-violet-950 transition duration-300">
-                        Save as Draft
-                    </button>
-                </div>
-
-                <div>
-                    <AvailableCards />
-                </div>
-            </div>
+          <button className="px-5 py-3 rounded-lg bg-slate-900/90 text-sm text-slate-50 hover:bg-black transition duration-300">
+            <FaPaperPlane />
+          </button>
         </div>
-    )
-}
+
+        <div className="w-full">
+          <div className="rounded-xl border-2 border-gray-300 px-5 py-3 flex justify-between items-center text-[9pt]">
+            <div className="flex items-center space-x-2">
+              <span>
+                <FaCcMastercard />
+              </span>
+              <span>Debit</span>
+            </div>
+
+            <div className="flex items-center space-x-2">
+              <span>$10.432</span>
+              <span>
+                <FaCaretDown />
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div className="w-full flex items-center space-x-3">
+          <div className="text-[8.5pt] font-[450] h-[60pt] px-5 py-5 border-2 border-gray-200 bg-gray-50 text-gray-600  w-full rounded-xl">
+            <label htmlFor="#amount">Account Number ($)</label>
+            <input
+              type="amount"
+              className="h-full w-full text-lg font-bold text-gray-900 bg-transparent"
+              placeholder="$124"
+            />
+          </div>
+        </div>
+
+        <div className="w-full flex justify-center space-x-3 font-bold text-sm">
+          <button className="bg-slate-950 text-slate-50 px-4 py-2 w-full shadow-md rounded-lg hover:bg-slate-300 border-2 border-slate-950 hover:text-slate-950 transition duration-300">
+            Send Money
+          </button>
+
+          <button className="bg-slate-50 text-slate-950 px-4 py-2 w-full shadow-md rounded-lg hover:bg-slate-300 border-2 border-slate-950 hover:text-slate-950 transition duration-300">
+            Save as Draft
+          </button>
+        </div>
+
+        <div>
+          <AvailableCards />
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/components/navigation/dashboard/TopNav.jsx
+++ src/components/navigation/dashboard/TopNav.jsx
@@ -1,36 +1,45 @@
-import { FaBell, FaSearch } from 'react-icons/fa'
+import { FaBell, FaSearch } from "react-icons/fa";
 
 export const TopNav = () => {
-    return (
-        <div className="w-full flex justify-between items-center">
-            <div className="py-4">
-                <form>
-                    <div className="w-[200pt] min-h-[20pt] rounded-3xl bg-gray-100 px-3 py-2 text-gray-700 flex items-center space-x-3 text-sm">
-                        <button className="border-none bg-transparent">
-                            <FaSearch />
-                        </button>
-                        <input type="text" className="border-none bg-transparent w-full hover:border-none focus:border-none focus-within:border-none py-1 px-2 font-[450]" placeholder="Search" />
-                    </div>
-                </form>
-            </div>
+  return (
+    <div className="w-full flex justify-between items-center">
+      <div className="py-4">
+        <form>
+          <div className="w-[200pt] min-h-[20pt] rounded-3xl bg-gray-100 px-3 py-2 text-gray-700 flex items-center space-x-3 text-sm">
+            <button className="border-none bg-transparent">
+              <FaSearch />
+            </button>
+            <input
+              type="text"
+              className="border-none bg-transparent w-full hover:border-none focus:border-none focus-within:border-none py-1 px-2 font-[450]"
+              placeholder="Search"
+            />
+          </div>
+        </form>
+      </div>
 
-            <div className="w-full hidden md:flex md:justify-end md:items-center px-5 md:space-x-10">
-                    <div className="flex justify-center items-center h-full">
-                        <select className="font-[450] border-none bg-transparent text-sm">
-                            <option value="EN">EN</option>
-                        </select>
-                    </div>
-
-                    <div className="flex justify-center items-center h-full text-xl">
-                        <button className="">
-                            <FaBell />
-                        </button>
-                    </div>
-
-                    <div className="flex justify-center items-center h-full">
-                        <img src="./download.jpeg" alt="Profile" className="rounded-full h-[40pt] w-[40pt]" />
-                    </div>
-            </div>
+      <div className="w-full hidden md:flex md:justify-end md:items-center px-5 md:space-x-10">
+        <div className="flex justify-center items-center h-full">
+          <select className="font-[450] border-none bg-transparent text-sm">
+            <option value="EN">EN</option>
+          </select>
         </div>
-    )
-}
+
+        <div className="flex justify-center items-center h-full text-xl">
+          <button className="">
+            <FaBell />
+          </button>
+        </div>
+
+        <div className="flex justify-center items-center h-full">
+          <img
+            // src="download.jpeg"
+            src="https://thispersondoesnotexist.com"
+            alt="Profile"
+            className="rounded-full h-[40pt] w-[40pt]"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
